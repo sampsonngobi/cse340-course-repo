d73131604e765a739829ec7551c664aa2d792c1a
dungasamsungo <sampsonngobi@gmail.com>
2026-07-10 19:20:15 -0300

    connect app to DB and generated organization content from the DB

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -35,8 +39,15 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
-    const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+    try {
+        const organizations = await getAllOrganizations();
+        const title = '....yes Our Partner Organizations';
+
+        res.render('organizations', { title, organizations });
+    } catch (error) {
+        console.error('Error loading organizations route:', error);
+        res.status(500).send('Error loading organizations data');
+    }
 });
 
 app.get('/projects', async (req, res) => {
@@ -48,7 +59,12 @@ app.get('/categories', async (req, res) => {
     const title = 'Here are the categories of our projects';
     res.render('categories', { title });
 });
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });
